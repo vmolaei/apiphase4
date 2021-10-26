a302e424d6359fca80d1612c0cd477f524d1f115
Vahid Molaei Bejarbaneh <91679087+vmolaei@users.noreply.github.com>
2021-10-26 16:13:00 +0330

    Second Commit:create Api Controllers

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    //Register Api --Post
+    public function register(Request $request){
+
+    }
+    //login Api --Post
+    public function login(Request $request){
+
+    }
+    //profile Api --Get
+    public function profile(){
+
+    }
+    //log out Api --Get
+    public function logout(){
+
+    }
+
+}
---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    //create method --Post
+    public function createBook(Request $request){
+
+    }
+    //list method --Get
+    public function listBooks(){
+
+    }
+    //single book method --Get
+    public function singleBook($book_id){
+
+    }
+    //update method --Post
+    public function updateBook(Request $request,$book_id){
+
+    }
+    //delete method --Get
+    public function deleteBook($book_id)
+
+}
